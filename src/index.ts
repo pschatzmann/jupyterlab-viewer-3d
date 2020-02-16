16775ecc93a3967eff891527d2eaefccaf71393c
Phil Schatzmann <phil.schatzmann@gmail.com>
2020-02-16 17:23:39 +0100

    Support multiple types

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const MIME_TYPES = ['model/stl','model/amf','model/obj','model/3mf','model/gcode
 export class Output3DWidget extends Widget implements IRenderMime.IRenderer {
     private scene = new THREE.Scene();
     private renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true  } );;
-    private camera = new THREE.PerspectiveCamera( 40, this.node.clientWidth/ this.node.clientHeight, 1, 100 );
+    private camera = new THREE.PerspectiveCamera( 40, this.node.clientWidth/ this.node.clientHeight, 1, 1000 );
     private controls =  new OrbitControls( this.camera, this.renderer.domElement );;
     private mimeType:string;
     private mesh:any;
@@ -57,7 +57,7 @@ export class Output3DWidget extends Widget implements IRenderMime.IRenderer {
           this.scene.add( this.camera );
 
           // add grid
-          var grid = new THREE.GridHelper( 50, 50, 0xffffff, 0x555555 );
+          var grid = new THREE.GridHelper( 200, 20, 0xffffff, 0x555555 );
           grid.rotateOnAxis( new THREE.Vector3( 1, 0, 0 ), 90 * ( Math.PI / 180 ) );
           this.scene.add( grid );
 
@@ -112,6 +112,39 @@ export class Output3DWidget extends Widget implements IRenderMime.IRenderer {
         requestAnimationFrame(this.animate.bind(this));
     }
 
+    fitCameraToObject(obj:any, camera:THREE.PerspectiveCamera, controls:any) {
+        var offset =  1.25;
+        const boundingBox = new THREE.Box3();
+    
+        // get bounding box of object - this will be used to setup controls and camera
+        boundingBox.setFromObject( obj );
+        var center = new THREE.Vector3();
+        boundingBox.getCenter(center);
+        var size = new THREE.Vector3();
+        boundingBox.getSize(size);
+    
+        // get the max side of the bounding box (fits to width OR height as needed )
+        const maxDim = Math.max( size.x, size.y, size.z );
+        const fov = this.camera.fov * ( Math.PI / 180 );
+        let cameraZ = Math.abs( maxDim / 4 * Math.tan( fov * 2 ) );
+    
+        cameraZ *= offset; // zoom out a little so that objects don't fill the scree
+        camera.position.z = cameraZ;
+        const minZ = boundingBox.min.z;
+        const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
+        camera.far = cameraToFarEdge * 3;
+        camera.updateProjectionMatrix();
+    
+        // set camera to rotate around center of loaded object
+        controls.target = center;
+
+        // prevent camera from zooming out far enough to create far plane cutoff
+        controls.maxDistance = cameraToFarEdge * 2;
+        controls.saveState();
+  
+    }
+  
+
 
     load(dataUrl:string) {
         var loader = this.getLoader(this.mimeType);
@@ -138,11 +171,12 @@ export class Output3DWidget extends Widget implements IRenderMime.IRenderer {
               self.mesh.rotation.y = Math.PI;   // rotate the model
             
               // pull the camera away so that it is a reasonable size
-              var largestDimension = Math.max(box.max.x, box.max.y, box.max.z)
-              self.camera.position.z = largestDimension * 10;
+              //var largestDimension = Math.max(box.max.x, box.max.y, box.max.z)
+              //self.camera.position.z = largestDimension * 10;
+              self.fitCameraToObject(self.mesh, self.camera, self.controls);
               
               //The focus point of the controls,
-              self.controls.target.set( 0, 0, 0 );
+              //self.controls.target.set( 0, 0, 0 );
 
               // add object to screen
               self.scene.add( self.mesh );
